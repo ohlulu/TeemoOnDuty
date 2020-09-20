a63a91d593bb38265de0e84639e57c853ecccfea
ohlulu <z30262226@gmail.com>
2020-09-20 18:41:25 +0800

    Fist

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
  */
 module.exports = app => {
   // Your code here
-  app.log.info('Yay, the app was loaded!')
+  app.log.info('Yay, Teemo on duty!')
 
-  app.on('issues.opened', async context => {
+  app.on('pull_request_review', async context => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     return context.github.issues.createComment(issueComment)
   })
